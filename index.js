22f10a60ef63da7c6c2b9333657aa6491850bfbc
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-09-22 22:01:24 +0530

    error static css done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.set('view engine',"ejs");
 
-app.use(express.static("public"));
+app.use(express.static("public", { "content-type": "text/css" }));
 app.use(bodyParser.urlencoded({extended:true}));
 
 
